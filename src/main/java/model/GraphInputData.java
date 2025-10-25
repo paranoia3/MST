3e5728af495685072032040248d66e3102816fd0
erkosh <erkegalielbosynovic@gmail.com>
2025-10-26 01:28:00 +0500

    master

---
+++ src/main/java/model/GraphInputData.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.util.List;
+
+public class GraphInputData {
+    private List<GraphDefinition> graphs;
+
+    public List<GraphDefinition> getGraphs() { return graphs; }
+    public void setGraphs(List<GraphDefinition> graphs) { this.graphs = graphs; }
+}
